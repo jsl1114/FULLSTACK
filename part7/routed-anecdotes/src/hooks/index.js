09c7b6dc55bb44afab3960031ae2dee7c9d6ce12
jsl1114 <ljs11141118@gmail.com>
2024-08-06 12:56:57 -0400

    7.7

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const clear = () => {
+    setValue('')
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    clear,
+  }
+}
